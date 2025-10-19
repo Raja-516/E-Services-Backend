fc4902cc08655fa14b8da4724e9b2cc5030d59dd
Raja-516 <akularaja44@gmail.com>
2025-10-19 23:13:17 +0530

    updated the Patient Appointments s

--- routes/appointment_routes.js
+++ routes/appointment_routes.js
@@ -13,7 +13,7 @@ const verifyToken = require("../middleware/authMiddleware"); // ✅ Correct path
 // ✅ Routes
 router.post("/", verifyToken, createAppointment);
 router.get("/patient/:patientId", verifyToken, getAppointmentsByPatient);
-router.get("/patient/:patientId", verifyToken, getAppointmentsByPatient);
+
 
 router.get("/doctor/:doctorId", verifyToken, getAppointmentsByDoctor);
 router.put("/:id/prescription", verifyToken, updatePrescription);
